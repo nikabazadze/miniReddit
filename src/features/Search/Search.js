46e84953287356f3e11832c9421103e74bb2c3c8
Nika Bazadze <nikabazadze@gmail.com>
2023-07-31 21:23:23 -0400

    Widen search input when in focus for mobile phone screens

--- src/features/Search/Search.js
+++ src/features/Search/Search.js
@@ -11,6 +11,45 @@ function Search() {
     const dispatch = useDispatch();
     const inputRef = useRef(null);
 
+    /**
+     * Handles click event for mobile phone screen.
+     * When clicked on, search bar becomes wider to see input text clearly 
+     */
+    function handleClick() {
+        if (window.innerWidth < 411) {
+            inputRef.current.focus();   // When clicking search icon instead of input field, it does not give input field focus without this code
+            widenSearch(true);
+        }
+    }
+
+    /**
+     * Fires when search input field loses focus. When fired it shrinks search bar to its normal size
+     * @param {Object} e 
+     */
+    function handleBlur(e) {
+        if ((window.innerWidth < 411) && (e.target.offsetWidth > (window.innerWidth * 0.4))) {
+            widenSearch(false);
+        }
+    }
+
+    /**
+     * Makes search bar wider or shrinks it to its normal size depending on boolean argument "grow"
+     * @param {boolean} grow 
+     */
+    function widenSearch(grow) {
+        const searchContainer = document.getElementById("searchBar");
+        const icon = document.getElementById("search-icon");
+        const input = document.getElementById("search-input");
+        searchContainer.style.width = grow ? "93%" : "40%";
+        searchContainer.style.transition = "width 0.12s linear";
+        input.style.marginRight = grow ? "0" : "1.25rem";
+        icon.style.display = grow ? "none" : "block";
+    }
+
+    /**
+     * Dispatches search term to the redux store
+     * @param {Object} e 
+     */
     function handleKeyDown(e) {
         if (e.key === "Enter") {
             e.preventDefault();
@@ -21,9 +60,10 @@ function Search() {
     }
 
     return (
-        <div className="searchBar">
-            <img src={searchIcon} className="search-icon" alt="search icon" />
-            <input  
+        <div id="searchBar" onClick={handleClick} onBlur={handleBlur} >
+            <img src={searchIcon} id="search-icon" alt="search icon" />
+            <input
+                id="search-input"
                 type="text"
                 value={localSearchTerm}
                 ref={inputRef}
