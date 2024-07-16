007388acd8289c104136938418107b29fba16bbe
Nika Bazadze <nikabazadze@gmail.com>
2024-07-16 12:53:58 -0400

    Added post button

--- src/components/PostCreator/PostCreator.js
+++ src/components/PostCreator/PostCreator.js
@@ -3,10 +3,15 @@ import React, { useState } from "react";
 import './PostCreator.css';
 import PostTypeChooser from "./PostTypeChooser/PostTypeChooser";
 import PostTitleChooser from "./PostTitleChooser/PostTitleChooser";
+import PostContentCreator from "./PostContentCreator/PostContentCreator";
 
 function PostCreator() {
-    const [ contentType, setContentType ] = useState("post");
+    const [ contentType, setContentType ] = useState("text");
     const [ postTitle, setPostTitle ] = useState("");
+
+    const handlePost = () => {
+
+    }
 
     return (
         <div id="post-creator">
@@ -15,6 +20,8 @@ function PostCreator() {
             <div className="post-container">
                 <PostTypeChooser contentType={contentType} setContentType={setContentType} />
                 <PostTitleChooser postTitle={postTitle} setPostTitle={setPostTitle} />
+                <PostContentCreator contentType={contentType} />
+                <div className="button-wrapper"><button onClick={handlePost}>Post</button></div>
             </div>
         </div>
     )
