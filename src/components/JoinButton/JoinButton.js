ee3f1c5b037f2f4b1535dd4a2d72307b68b290bc
Nika Bazadze <nikabazadze@gmail.com>
2024-07-17 12:29:39 -0400

    Use AlertDialog component

--- src/components/JoinButton/JoinButton.js
+++ src/components/JoinButton/JoinButton.js
@@ -1,47 +1,19 @@
-import React from "react";
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import React, { useState } from "react";
+import AlertDialog from "../AlertDialog/AlertDialog";
 import './JoinButton.css';
 
 function JoinButton({cta, placement, containerStyle}) {
-    const [ open, setOpen ] = React.useState(false);
-
-    // Handles opening a dialog window when clicking on the cta button
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    // Handles closing a dialog window when clicking on the cta button
-    const handleClose = () => {
-      setOpen(false);
-    };
+    const [ openDialog, setOpenDialog ] = useState(false);
 
     return (
         <div className={containerStyle}>
-            <button className={`join-button ${placement}`} onClick={handleClickOpen}>{cta}</button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Can not sign up!"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        This is a front-end only website
-                        without any server and database.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>close</Button>
-                </DialogActions>
-            </Dialog>
+            <button className={`join-button ${placement}`} onClick={() => setOpenDialog(true)}>{cta}</button>
+            {openDialog &&  <AlertDialog 
+                                title="Can not sign up!"
+                                content="This is a front-end only website without any server and database."
+                                onClose={setOpenDialog}
+                            />
+            }
         </div>
     )
 }
