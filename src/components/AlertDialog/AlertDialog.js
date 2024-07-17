438281192776c7fb490bf26cd8fa2afec41233d7
Nika Bazadze <nikabazadze@gmail.com>
2024-07-17 12:28:42 -0400

    Created dialog component for showing alert

---
+++ src/components/AlertDialog/AlertDialog.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function AlertDialog({title, content, onClose}) {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+    onClose(false);
+  };
+
+  return (
+    <div style={{position: "absolute"}}>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {title && <DialogTitle id="alert-dialog-title">
+                    {title}
+                  </DialogTitle>
+        }
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {content}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AlertDialog;
